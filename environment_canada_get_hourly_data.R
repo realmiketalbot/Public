9b3dec22a044e328c1b5d5c7cba66408528b1120
Mike Talbot <29737293+realmiketalbot@users.noreply.github.com>
2020-09-10 08:40:17 -0500

    Initial commit

---
+++ environment_canada_get_hourly_data.R
@@ -0,0 +1,28 @@
+# Download Environment Canada hourly weather data
+
+# Mirrors the Linux CLI instructions found here: https://drive.google.com/drive/folders/1WJCDEU34c60IfOnG4rv5EPZ4IhhW9vZH
+
+library(tidyr)
+
+years <- c(1998:2008)
+months <- c(1:12)
+
+station.code <- "1706"
+
+#station list: https://docs.google.com/spreadsheets/d/1MmbCdB16fR0Q6KNA5Q2k4ddnVW9ZzE6AIyC9K1AokhQ/edit#gid=806259678
+#use Station ID column
+
+data.list <- list()
+
+for (year in years) {
+  for (month in months) {
+    index <- str_c(year, month, sep="-")
+    download.file(sprintf("https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=1706&Year=%s&Month=%s&Day=14&timeframe=1&submit= Download+Data", year, month),
+                  method = "wget", extra = "--content-disposition", destfile="temp.csv")
+    data.list[[index]] <- read_csv("temp.csv", col_types = cols(.default = "c"))
+  }
+}
+
+data <- bind_rows(data.list)
+
+write_csv(data, str_glue("EnvironmentCanada_Weather_Station{station.code}_From{min(years)}_To{max(years)}.csv"))
