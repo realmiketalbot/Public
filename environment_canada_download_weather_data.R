cca4cfde66508ecee54665e2333fe65add829d65
Mike Talbot <29737293+realmiketalbot@users.noreply.github.com>
2020-12-17 12:47:43 -0600

    Added ability to download either hourly or daily data, fixed bug in code for naming csv output file

--- environment_canada_download_weather_data.R
+++ environment_canada_download_weather_data.R
@@ -1,6 +1,8 @@
-# Download Environment Canada hourly weather data
+# Download Environment Canada weather data
 
 # Mirrors the Linux CLI instructions found here: https://drive.google.com/drive/folders/1WJCDEU34c60IfOnG4rv5EPZ4IhhW9vZH
+
+setwd("/mnt/hdd/git/Public/data")
 
 library(tidyverse)
 
@@ -8,6 +10,8 @@ years <- c(2015:2015)
 months <- c(5:10)
 
 station <- "50132"
+
+timeframe <- 1 #1 for hourly, 2 for daily
 
 #station list: https://docs.google.com/spreadsheets/d/1MmbCdB16fR0Q6KNA5Q2k4ddnVW9ZzE6AIyC9K1AokhQ/edit#gid=806259678
 #use Station ID column
@@ -17,7 +21,7 @@ data.list <- list()
 for (year in years) {
   for (month in months) {
     index <- str_c(year, month, sep="-")
-    download.file(sprintf("https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=%s&Year=%s&Month=%s&Day=14&timeframe=1&submit= Download+Data", station, year, month),
+    download.file(sprintf("https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=%s&Year=%s&Month=%s&Day=14&timeframe=%s&submit= Download+Data", station, year, month, timeframe),
                   method = "wget", extra = "--content-disposition", destfile="temp.csv")
     data.list[[index]] <- read_csv("temp.csv", col_types = cols(.default = "c"))
   }
@@ -25,4 +29,10 @@ for (year in years) {
 
 data <- bind_rows(data.list)
 
-write_csv(data, str_glue("EnvironmentCanada_Weather_Station{station.code}_From{min(years)}_To{max(years)}.csv"))
+if (timeframe==1){
+  timestep <- "Hourly"
+} else {
+  timestep <- "Daily"
+}
+
+write_csv(data, str_glue("EnvironmentCanada_Weather_Station{station}_{timestep}_From{min(years)}_To{max(years)}.csv"))
